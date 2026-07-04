7a902bbd1ce04d65f4d42d50b4f5e9483e580a48
Alisah-Ozcan <alisahozcan97@gmail.com>
2026-07-04 15:06:28 +0300

    Add other external product functions to test case.

--- tests/unit/core/ggsw/test_ggsw_external_product.c
+++ tests/unit/core/ggsw/test_ggsw_external_product.c
@@ -18,7 +18,7 @@
 #include "gpu/host/ggsw_external_product_gpu.h"
 #endif
 
-#define TIMING
+//#define TIMING
 // ---------------------------------------------------------------------------
 // Timing helpers — enabled with -DTIMING (cmake -DTIMING=ON)
 // ---------------------------------------------------------------------------
@@ -120,6 +120,157 @@ PvdaParamTest(ggsw_external_product, without_error, default_params_fn)
 	delete_glwe(ext_prod_observed);
 	delete_glwe(glwe_tilde);
 	delete_ggsw(ggsw);
+
+	delete_glwe_secret_key(sk_ggsw);
+	delete_glwe_secret_key_prepared(sk_glwe_prep);
+
+	DELETE_PVDA_PARAMS_GGSW;
+}
+
+/** Prepared GGSW external product — result in DFT domain, then converted back */
+PvdaParamTest(ggsw_external_product, to_dft_without_error, default_params_fn)
+{
+	INIT_PVDA_PARAMS_GGSW(param);
+
+	params_glwe->fast_uniform_nb_bits = 0;
+	sigma                             = 0;
+	double err_length                 = glwe_bivariate_epsilon(params_glwe) + 3 * sigma + 3 * DBL_EPSILON;
+	double critical_err_length        = glwe_bivariate_epsilon(params_glwe) + 5 * sigma + 5 * DBL_EPSILON;
+
+	GLWESecretKey* sk_ggsw              = alloc_glwe_secret_key(params_glwe);
+	GLWESecretKeyPrepared* sk_glwe_prep = alloc_glwe_secret_key_prepared(params_glwe);
+	GGSWCiphertext* ggsw                = new_ggsw(params_ggsw);
+	GGSWCiphertextPrep* ggsw_prep       = new_ggsw_prep(params_ggsw);
+	GLWECiphertext* glwe_tilde          = new_glwe(params_glwe);
+	GLWECiphertextDFT* result_dft       = new_glwe_dft(params_glwe);
+	GLWECiphertext* ext_prod_observed   = new_glwe(params_glwe);
+	PolyUniv* u_univ                    = new_univ(params_glwe);
+	PolyBiv* m                          = new_biv(params_glwe);
+
+	PolyBiv* phase_observed           = new_biv(params_glwe);
+	PolyUnivRnX* um_observed_univ_rnx = new_univ_rnx(params_glwe);
+	PolyUnivDFT* u_univ_dft           = new_univ_dft(module);
+	PolyBivDFT* um_dft                = new_biv_dft(params_glwe);
+	PolyBiv* um                       = new_biv(params_glwe);
+	PolyUnivRnX* um_univ_rnx          = new_univ_rnx(params_glwe);
+
+	uniform_glwe_secret_key(module, sk_ggsw, 3);
+	glwe_sk_prepare(module, sk_glwe_prep, sk_ggsw);
+
+	uniform_random_pol_znx(u_univ, params_glwe->nn, params_glwe->kappa);
+	uniform_random_biv_poly(params_glwe, m, 1);
+
+	glwe_secret_encrypt_phase(module, glwe_tilde, sk_glwe_prep, m);
+	ggsw_secret_encrypt(module, ggsw, sk_glwe_prep, u_univ);
+	ggsw_prepare(module, ggsw_prep, ggsw);
+
+	PVDA_TIME_START(external_product_to_dft);
+	ggsw_external_product_to_dft(module, result_dft, glwe_tilde, ggsw_prep);
+	PVDA_TIME_END(external_product_to_dft, params_glwe, params_ggsw);
+
+	glwe_dft_to_coef(module, ext_prod_observed, result_dft);
+	normalize_glwe(module, ext_prod_observed, ext_prod_observed);
+	glwe_secret_decrypt(module, phase_observed, sk_glwe_prep, ext_prod_observed);
+	biv_to_univ_rnx(params_glwe, um_observed_univ_rnx, phase_observed);
+
+	//Computes u*m manually
+	univ_coefs_to_dft(module, u_univ_dft, u_univ);
+	pvda_svp_apply_dft(module, um_dft, ggsw_params_l_tilde_a(params_ggsw), u_univ_dft, m);
+	biv_dft_to_coefs(module, params_glwe, um, um_dft);
+	pvda_vec_znx_normalize_base2k(module, params_glwe->kappa, um, um);
+	biv_to_univ_rnx(params_glwe, um_univ_rnx, um);
+
+	pvda_assert_polynomial_distance(params_glwe, um_observed_univ_rnx, um_univ_rnx, err_length, critical_err_length);
+
+	// Clean up
+	delete_biv(m);
+	delete_univ(u_univ);
+	delete_univ_dft(u_univ_dft);
+	delete_biv(phase_observed);
+	delete_biv(um);
+	delete_univ_rnx(um_univ_rnx);
+	free(um_dft);
+	delete_univ_rnx(um_observed_univ_rnx);
+
+	delete_glwe(ext_prod_observed);
+	delete_glwe_dft(result_dft);
+	delete_glwe(glwe_tilde);
+	delete_ggsw(ggsw);
+	delete_ggsw_prep(ggsw_prep);
+
+	delete_glwe_secret_key(sk_ggsw);
+	delete_glwe_secret_key_prepared(sk_glwe_prep);
+
+	DELETE_PVDA_PARAMS_GGSW;
+}
+
+/** Prepared GGSW external product — result directly in coefficient domain */
+PvdaParamTest(ggsw_external_product, prepared_without_error, default_params_fn)
+{
+	INIT_PVDA_PARAMS_GGSW(param);
+
+	params_glwe->fast_uniform_nb_bits = 0;
+	sigma                             = 0;
+	double err_length                 = glwe_bivariate_epsilon(params_glwe) + 3 * sigma + 3 * DBL_EPSILON;
+	double critical_err_length        = glwe_bivariate_epsilon(params_glwe) + 5 * sigma + 5 * DBL_EPSILON;
+
+	GLWESecretKey* sk_ggsw              = alloc_glwe_secret_key(params_glwe);
+	GLWESecretKeyPrepared* sk_glwe_prep = alloc_glwe_secret_key_prepared(params_glwe);
+	GGSWCiphertext* ggsw                = new_ggsw(params_ggsw);
+	GGSWCiphertextPrep* ggsw_prep       = new_ggsw_prep(params_ggsw);
+	GLWECiphertext* glwe_tilde          = new_glwe(params_glwe);
+	GLWECiphertext* ext_prod_observed   = new_glwe(params_glwe);
+	PolyUniv* u_univ                    = new_univ(params_glwe);
+	PolyBiv* m                          = new_biv(params_glwe);
+
+	PolyBiv* phase_observed           = new_biv(params_glwe);
+	PolyUnivRnX* um_observed_univ_rnx = new_univ_rnx(params_glwe);
+	PolyUnivDFT* u_univ_dft           = new_univ_dft(module);
+	PolyBivDFT* um_dft                = new_biv_dft(params_glwe);
+	PolyBiv* um                       = new_biv(params_glwe);
+	PolyUnivRnX* um_univ_rnx          = new_univ_rnx(params_glwe);
+
+	uniform_glwe_secret_key(module, sk_ggsw, 3);
+	glwe_sk_prepare(module, sk_glwe_prep, sk_ggsw);
+
+	uniform_random_pol_znx(u_univ, params_glwe->nn, params_glwe->kappa);
+	uniform_random_biv_poly(params_glwe, m, 1);
+
+	glwe_secret_encrypt_phase(module, glwe_tilde, sk_glwe_prep, m);
+	ggsw_secret_encrypt(module, ggsw, sk_glwe_prep, u_univ);
+	ggsw_prepare(module, ggsw_prep, ggsw);
+
+	PVDA_TIME_START(external_product_prepared);
+	ggsw_external_product(module, ext_prod_observed, glwe_tilde, ggsw_prep);
+	PVDA_TIME_END(external_product_prepared, params_glwe, params_ggsw);
+
+	normalize_glwe(module, ext_prod_observed, ext_prod_observed);
+	glwe_secret_decrypt(module, phase_observed, sk_glwe_prep, ext_prod_observed);
+	biv_to_univ_rnx(params_glwe, um_observed_univ_rnx, phase_observed);
+
+	//Computes u*m manually
+	univ_coefs_to_dft(module, u_univ_dft, u_univ);
+	pvda_svp_apply_dft(module, um_dft, ggsw_params_l_tilde_a(params_ggsw), u_univ_dft, m);
+	biv_dft_to_coefs(module, params_glwe, um, um_dft);
+	pvda_vec_znx_normalize_base2k(module, params_glwe->kappa, um, um);
+	biv_to_univ_rnx(params_glwe, um_univ_rnx, um);
+
+	pvda_assert_polynomial_distance(params_glwe, um_observed_univ_rnx, um_univ_rnx, err_length, critical_err_length);
+
+	// Clean up
+	delete_biv(m);
+	delete_univ(u_univ);
+	delete_univ_dft(u_univ_dft);
+	delete_biv(phase_observed);
+	delete_biv(um);
+	delete_univ_rnx(um_univ_rnx);
+	free(um_dft);
+	delete_univ_rnx(um_observed_univ_rnx);
+
+	delete_glwe(ext_prod_observed);
+	delete_glwe(glwe_tilde);
+	delete_ggsw(ggsw);
+	delete_ggsw_prep(ggsw_prep);
 
 	delete_glwe_secret_key(sk_ggsw);
 	delete_glwe_secret_key_prepared(sk_glwe_prep);
